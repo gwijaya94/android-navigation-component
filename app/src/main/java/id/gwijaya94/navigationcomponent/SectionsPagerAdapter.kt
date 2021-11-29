0be0e4b39603f685daf90370b5b609c60988cd14
Gilbert <gilbert.wijaya@ai.astra.co.id>
2021-11-30 01:56:31 +0700

    - connect tablayout activity with drawer - create tabLayout - create SectionsPagerAdapter with modified tablist and methods

---
+++ app/src/main/java/id/gwijaya94/navigationcomponent/SectionsPagerAdapter.kt
@@ -0,0 +1,15 @@
+package id.gwijaya94.navigationcomponent
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import id.gwijaya94.navigationcomponent.ui.home.HomeFragment
+
+class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
+    private var tabList = listOf(HomeFragment(), ProfileFragment())
+
+    override fun getItemCount(): Int = tabList.size
+
+    override fun createFragment(position: Int): Fragment = tabList[position]
+
+}
---
+++ app/src/main/java/id/gwijaya94/navigationcomponent/ViewPagerActivity.kt
@@ -0,0 +1,33 @@
+package id.gwijaya94.navigationcomponent
+
+import android.os.Bundle
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayoutMediator
+import id.gwijaya94.navigationcomponent.databinding.ActivityViewPagerBinding
+
+class ViewPagerActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityViewPagerBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityViewPagerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this)
+        binding.viewPager.adapter = sectionsPagerAdapter
+
+        TabLayoutMediator(binding.tabs, binding.viewPager) { tab, position ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
+
+        supportActionBar?.elevation = 0f
+    }
+
+    companion object {
+        @StringRes
+        private val TAB_TITLES = intArrayOf(
+            R.string.tab_text_1,
+            R.string.tab_text_2
+        )
+    }
+}
